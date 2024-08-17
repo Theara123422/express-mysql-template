a8a741b615214c19ed3855aae8b8b9cdee82c11b
Th1234wee <theara6574@gmail.com>
2024-08-17 12:09:16 +0700

    Finished Create UI

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import blog from './blog.js';
+const app = express();
+
+// setting up ejs(embeded javascript)
+app.set('view engine' , 'ejs'); //set our view engine to ejs
+app.set('views','views');//set ./views folder as views
+
+app.get('/' , (request,response) => {
+    response.render('index.ejs',{
+        data : blog
+    });
+});
+
+
+
+
+app.listen(3000 , () => {
+    console.log(`Server is running on http://localhost:3000`);
+})
