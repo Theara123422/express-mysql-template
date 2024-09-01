5f016b5771c68d28f765277a499f6f70c6959b55
Th1234wee <theara6574@gmail.com>
2024-09-01 12:16:45 +0700

    Finished Edit Blog

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,5 +1,33 @@
 import { Router } from "express";
+import pool from "../database/db_connect.js";
 
 const blogRoute = Router();
-
+//redirect to add blog page
+blogRoute.get('/addblogpage' , (request,response) =>{
+    response.render('add.ejs');
+});
+//add new blog route
+blogRoute.post('/createblog' , (request,response)=> {
+    const { title , description } = request.body;
+    console.log(title+description);
+    
+    pool.query(`INSERT INTO blog (title,description) VALUES(?,?)`,[title,description],(error,result)=>{
+        if(error) return response.status(500).json({
+            message : "Something went wrong"
+        })
+        response.redirect('/');
+    });
+});
+//handle route delete blog
+blogRoute.post('/removeblog/:id' , (request,response) => {
+    const { id } = request.params;
+    console.log(id);
+    
+    pool.query(`DELETE FROM blog WHERE id = ?`,id,(error,result) => {
+        if(error) return response.status(500).json({
+            message : "Something went wrong"
+        });
+        response.redirect('/');
+    })
+})
 export default blogRoute;
